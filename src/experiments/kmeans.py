5e2182e63efed942efcb80375c4373faca3d3a26
Zachary Zipper <zezip@umich.edu>
2021-04-29 13:56:34 -0400

    uploaded final edits to kmeans code and results. Running ./src/experiments/kmeans.py will run all experiments and generate all graphs in 'Evaluation'

--- src/experiments/kmeans.py
+++ src/experiments/kmeans.py
@@ -1,59 +1,274 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
 import time
 import tqdm
 import copy
+import seaborn as sns
 import matplotlib.pyplot as plt
-sns.set(rc={'figure.figsize':(5,5)})
-sns.color_palette("mako", as_cmap=True)
+from pathlib import Path
+import os, os.path
+import errno
 from collections import defaultdict, Counter
 from sklearn.cluster import MiniBatchKMeans
-from sklearn.model_selection import StratifiedKFold
 from sklearn.decomposition import PCA
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 # from data_primer_modified import standardizeDataDims as data
 from data_primer import standardizeDataDims as data
 
+DIR = Path(os.path.abspath('')).resolve()
+ROOT = DIR.parent.parent
+EVALUATION = ROOT/"Evaluation"
+OUTDIR = EVALUATION/"kmeans"
+
 SEED = 1
 NUM_DRIVERS = 13
-TEST_LATENCY_AND_RAM = True
-N_CLUSTERS = 7
-OUTFILE_NAME = 'kmeans_results.csv'
+TEST_LATENCY_AND_RAM = False
+PCA_COMPONENTS = 2
+
+# 1st dict is FE choice
+# 2nd dict is PCA choice
+N_CLUSTERS_DICT = {
+    True : {
+        True : 7,   # FE + PCA
+        False : 8,  # FE
+    },
+    False : {
+        True : 5,   # PCA
+        False : 12  #
+    }
+}
 
 def main():
+    try_mkdir(str(OUTDIR))
+    run_graph_experiment()
+    run_full_experiment()
+    make_pairwise_graphs()
+    make_pairwise_grid()
+    make_elbow_grid()
+
+def run_graph_experiment():
+    for fe_extract in [True, False]:
+        for use_pca in [True, False]:
+            graph_experiment(fe_extract, use_pca)
+
+def make_pairwise_grid():
+    fig, axes = plt.subplots(4, 2, figsize=(10, 12))
+    fig.suptitle('Cluster & Label Comparison')
+    plot_index = 0
+    for num_classes in [2, 3]:
+        for fe_extract in [True, False]:
+
+            print('Pulling data...')
+            _, XDrivers, _, _, YDrivers = data()
+            XDrivers, _, YDrivers, _ = prepareData(XDrivers, YDrivers, num_classes)
+            X_Drivers = copy.deepcopy(XDrivers)
+            Y_Drivers = copy.deepcopy(YDrivers)
+
+            if fe_extract:
+                print('Feature extraction...')
+                for i in range(NUM_DRIVERS):
+                    X_Drivers[i] = feature_extraction(XDrivers[i])
+                    Y_Drivers[i] = new_label(YDrivers[i], num_classes)
+
+            X = np.concatenate(X_Drivers)
+            y = np.concatenate(Y_Drivers)
+
+            print('PCA...')
+            pca = PCA(n_components=2)
+            embedded_X = pca.fit_transform(X)
+
+            kmeans = MiniBatchKMeans(n_clusters=N_CLUSTERS_DICT[fe_extract][True], init='k-means++', max_iter=1000, batch_size=10000, compute_labels=True, random_state=SEED)
+            kmeans.fit(embedded_X)
+
+            df = pd.DataFrame({
+                'pca0' : embedded_X[:, 0],
+                'pca1' : embedded_X[:, 1],
+                'Cluster' : kmeans.labels_,
+                'Label' : [int(item) for item in y]
+            })
+
+            ax = axes[plot_index]
+            ax[0].set_ylabel(f"{num_classes}-Class {'w/FE' if fe_extract else ''}", rotation='vertical', size='large')
+            ax[1].set_ylabel(' ')
+            ax[0].set_xlabel(' ')
+            ax[1].set_xlabel(' ')
+
+            sns.scatterplot(ax=ax[0], data=df, x='pca0', y='pca1', hue='Cluster', palette='mako', linewidth=0.05, edgecolors=None)
+            sns.scatterplot(ax=ax[1], data=df, x='pca0', y='pca1', hue='Label', palette='mako', linewidth=0.05, edgecolors=None)
+            ax[0].get_legend().remove()
+            ax[1].get_legend().remove()
+            plot_index += 1
+
+    axes[0][0].set_title('Cluster')
+    axes[0][1].set_title('Label')
+    plt.savefig(f"{str(OUTDIR)}/all_scatter.png", bbox_inches='tight')
+    plt.close()
+
+def make_pairwise_graphs():
+    for num_classes in [2, 3]:
+        for fe_extract in [True, False]:
+
+            print('Pulling data...')
+            _, XDrivers, _, _, YDrivers = data()
+            XDrivers, _, YDrivers, _ = prepareData(XDrivers, YDrivers, num_classes)
+            X_Drivers = copy.deepcopy(XDrivers)
+            Y_Drivers = copy.deepcopy(YDrivers)
+
+            if fe_extract:
+                print('Feature extraction...')
+                for i in range(NUM_DRIVERS):
+                    X_Drivers[i] = feature_extraction(XDrivers[i])
+                    Y_Drivers[i] = new_label(YDrivers[i], num_classes)
+
+            X = np.concatenate(X_Drivers)
+            y = np.concatenate(Y_Drivers)
+
+            print('PCA...')
+            pca = PCA(n_components=2)
+            embedded_X = pca.fit_transform(X)
+
+            kmeans = MiniBatchKMeans(n_clusters=N_CLUSTERS_DICT[fe_extract][True], init='k-means++', max_iter=1000, batch_size=10000, compute_labels=True, random_state=SEED)
+            kmeans.fit(embedded_X)
+
+            df = pd.DataFrame({
+                'pca0' : embedded_X[:, 0],
+                'pca1' : embedded_X[:, 1],
+                'Cluster' : kmeans.labels_,
+                'Label' : [int(item) for item in y]
+            })
+            fig, axes = plt.subplots(1, 2, figsize=(10, 5), sharey=True)
+            fig.suptitle('Cluster & Label Comparison')
+
+            sns.scatterplot(ax=axes[0], data=df, x='pca0', y='pca1', hue='Cluster', palette="mako")
+            axes[0].set_title('Cluster')
+
+            sns.scatterplot(ax=axes[1], data=df, x='pca0', y='pca1', hue='Label', palette="mako")
+            axes[1].set_title('Label')
+            plt.savefig(f"{str(OUTDIR)}/{str(num_classes)}_{'fe_' if fe_extract else ''}scatter.png")
+            plt.close()
+
+def run_full_experiment():
     lat_arr = []
     prec_arr = []
     rec_arr = []
     f1_arr = []
     acc_arr = []
+
     classes_arr = []
     fe_arr = []
+    clusters_arr = []
+    pca_arr = []
 
     for num_classes in [2, 3]:
         for fe_extract in [True, False]:
-            lat, precision, recall, f1, accuracy = run_experiment(num_classes, fe_extract)
-            lat_arr.append(lat)
-            prec_arr.append(precision)
-            rec_arr.append(recall)
-            f1_arr.append(f1)
-            acc_arr.append(accuracy)
-            classes_arr.append(num_classes)
-            fe_arr.append(fe_extract)
+            for use_pca in [True, False]:
+                elbow_analysis_choice = N_CLUSTERS_DICT[fe_extract][use_pca]
+                for num_clusters in [elbow_analysis_choice, num_classes]:
+                    lat, precision, recall, f1, accuracy = run_experiment(num_classes, fe_extract, use_pca, num_clusters)
+
+                    lat_arr.append(lat)
+                    prec_arr.append(precision)
+                    rec_arr.append(recall)
+                    f1_arr.append(f1)
+                    acc_arr.append(accuracy)
+
+                    classes_arr.append(num_classes)
+                    clusters_arr.append(num_clusters)
+                    fe_arr.append(fe_extract)
+                    pca_arr.append(use_pca)
 
     pd.DataFrame({
         'number of classes' : classes_arr,
+        'number of clusters' : clusters_arr,
         'feature extraction used' : fe_arr,
+        'pca used' : pca_arr,
         'accuracy' : acc_arr,
         'precision' : prec_arr,
         'recall' : rec_arr,
         'f1': f1_arr,
         '500 prediction latency (ms)' : lat_arr,
-    }).to_csv(OUTFILE_NAME, index=False)
+    }).to_csv(str(OUTDIR/'kmeans_results.csv'), index=False)
+
+def make_elbow_grid():
+    fig, axes = plt.subplots(2, 2, figsize=(9, 9))
+    fig.suptitle('PCA')
+    fig.text(0.04, 0.5, 'FE', va='center', rotation='vertical', size='large')
+    plot_index = 0
+    for use_pca in [True, False]:
+        for fe_extract in [True, False]:
+            print('Pulling data...')
+            _, XDrivers, _, _, YDrivers = data()
+            # Num classes is arbitrary here, we don't use the y assignments
+            XDrivers, _, YDrivers, _ = prepareData(XDrivers, YDrivers, 2)
+            X_Drivers = copy.deepcopy(XDrivers)
+
+            if fe_extract:
+                print('Feature extraction...')
+                for i in range(NUM_DRIVERS):
+                    X_Drivers[i] = feature_extraction(XDrivers[i])
+
+            X_Drivers = np.concatenate(X_Drivers)
+
+            if use_pca:
+                pca = PCA(n_components=PCA_COMPONENTS)
+                X_Drivers = pca.fit_transform(X_Drivers)
+            
+            print('Making elbow graph...')
+            scores = []
+            clusters = []
+            for i in tqdm.tqdm(range(1, 30)):
+                kmeans = MiniBatchKMeans(n_clusters=i, init='k-means++', max_iter=1000, batch_size=10000, compute_labels=True, random_state=SEED)
+                kmeans.fit(X_Drivers)
+                score = kmeans.score(X_Drivers)
+                clusters.append(i)
+                scores.append(-score)
+            df = pd.DataFrame({
+                '# Clusters' : clusters,
+                'Loss' : scores
+            })
+
+            m, n = plot_index % 2, int(plot_index/2)
+            ax = axes[m][n]
+
+            if m == 0:
+                ax.set_title("Yes" if use_pca else "No", fontsize='large')
+            else:
+                ax.set_title(' ')
+            if n == 0:
+                ax.set_ylabel("Yes" if fe_extract else "No", fontsize='large')
+            else:
+                ax.set_ylabel(' ')
 
 
+            sns.lineplot(ax=ax, data=df, x='# Clusters', y='Loss', color='blue')
+            plot_index += 1
 
-def run_experiment(num_classes, fe_extract):
+    plt.savefig(f"{str(OUTDIR)}/all_elbows.png", bbox_inches='tight')
+    plt.close()
+
+def graph_experiment(fe_extract, use_pca):
+    print('Pulling data...')
+    _, XDrivers, _, _, YDrivers = data()
+    # Num classes is arbitrary here, we don't use the y assignments
+    XDrivers, _, YDrivers, _ = prepareData(XDrivers, YDrivers, 2)
+    X_Drivers = copy.deepcopy(XDrivers)
+
+    if fe_extract:
+        print('Feature extraction...')
+        for i in range(NUM_DRIVERS):
+            X_Drivers[i] = feature_extraction(XDrivers[i])
+
+    X_Drivers = np.concatenate(X_Drivers)
+
+    if use_pca:
+        pca = PCA(n_components=PCA_COMPONENTS)
+        X_Drivers = pca.fit_transform(X_Drivers)
+    
+    print('Making elbow graph...')
+    make_elbow_curve_graph(X_Drivers, fe_extract, use_pca)
+
+
+def run_experiment(num_classes, fe_extract, use_pca, n_clusters):
     print('Pulling data...')
     _, XDrivers, _, _, YDrivers = data()
     XDrivers, _, YDrivers, _ = prepareData(XDrivers, YDrivers, num_classes)
@@ -67,29 +282,28 @@ def run_experiment(num_classes, fe_extract):
             Y_Drivers[i] = new_label(YDrivers[i], num_classes)
 
     lat_test_set = XDrivers[0]
-    
-    print('Making elbow graph...')
-    make_elbow_curve_graph(X_Drivers, fe=fe_extract)
-
 
     print('Running LODO:')
-    return run_lodo(X_Drivers, Y_Drivers, lat_test_set, fe_extract)
+    return run_lodo(X_Drivers, Y_Drivers, lat_test_set, fe_extract, use_pca, n_clusters)
 
-
-def run_lodo(X_Drivers, Y_Drivers, lat_test_set, fe_extract):
+def run_lodo(X_Drivers, Y_Drivers, lat_test_set, fe_extract, use_pca, n_clusters):
     precision = []
     recall = []
     f1 = []
     accuracy = []
 
-    lat, res_usage = None, None
+    lat = None
 
     for iteration in tqdm.tqdm(range(NUM_DRIVERS)):
         # Get LODO data
         X_train, y_train, X_test, y_test = getLODOIterData(X_Drivers, Y_Drivers, iteration)
 
-        # kmeans = MiniBatchKMeans(n_clusters=NUM_CLASSES, init='k-means++', max_iter=1000, batch_size=10000, compute_labels=True, random_state=SEED)
-        kmeans = MiniBatchKMeans(n_clusters=N_CLUSTERS, init='k-means++', max_iter=1000, batch_size=10000, compute_labels=True, random_state=SEED)
+        if use_pca:
+            pca = PCA(n_components=PCA_COMPONENTS)
+            X_train = pca.fit_transform(X_train)
+            X_test = pca.transform(X_test)
+
+        kmeans = MiniBatchKMeans(n_clusters=n_clusters, init='k-means++', max_iter=1000, batch_size=10000, compute_labels=True, random_state=SEED)
         kmeans.fit(X_train)
         # Now that the model is fit, assign each centroid's label to be the dominant class (we're really just hoping this lines up with y)
         centroid_to_class_counters = defaultdict(lambda : Counter())
@@ -137,14 +351,13 @@ def latency(predict_func, X_in, fe_extract, iters=100000):
     print('Runtime (ms):', runtime)
     return runtime
 
-def make_elbow_curve_graph(X_Drivers, fe=True):
+def make_elbow_curve_graph(X_Drivers, fe, use_pca):
     scores = []
     clusters = []
-    all_X = np.concatenate(X_Drivers, axis=0)
     for i in tqdm.tqdm(range(1, 30)):
         kmeans = MiniBatchKMeans(n_clusters=i, init='k-means++', max_iter=1000, batch_size=10000, compute_labels=True, random_state=SEED)
-        kmeans.fit(all_X)
-        score = kmeans.score(all_X)
+        kmeans.fit(X_Drivers)
+        score = kmeans.score(X_Drivers)
         clusters.append(i)
         scores.append(-score)
     df = pd.DataFrame({
@@ -152,8 +365,8 @@ def make_elbow_curve_graph(X_Drivers, fe=True):
         'Loss' : scores
     })
     sns.lineplot(data=df, x='# Clusters', y='Loss', color='blue')
-    plt.savefig('fe_elbow_curve.png' if fe else 'elbow_curve.png')
-
+    plt.savefig(f"{str(OUTDIR)}/{'fe_' if fe else ''}{'pca_' if use_pca else ''}elbow.png")
+    plt.close()
 
 def prepareData(XDrivers, YDrivers, numClasses):
   """
@@ -313,6 +526,13 @@ def new_label(Y_driver,num_classes):
         end = start + 50
 
     return np.array(new_output)
+
+def try_mkdir(dirname):
+    try:
+        os.mkdir(dirname)
+    except OSError as exc:
+        if exc.errno != errno.EEXIST:
+            raise
 
 if __name__=="__main__":
     main()
